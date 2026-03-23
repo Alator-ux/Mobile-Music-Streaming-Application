0f1f430f0ab8775867576514db4e0d13a56245e5
nktfmnko <nktfmnko@gmail.com>
2026-03-23 12:58:45 +0300

    feat: добавлен пример локализации flutter приложения

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,14 +1,56 @@
+import 'package:client/l10n/gen/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: const HelloWorld()));
+  runApp(const TestApp());
 }
 
-class HelloWorld extends StatelessWidget {
-  const HelloWorld({super.key});
+class TestApp extends StatefulWidget {
+  const TestApp({super.key});
+
+  @override
+  State<TestApp> createState() => _TestAppState();
+}
+
+class _TestAppState extends State<TestApp> {
+  Locale _locale = const Locale('ru');
+
+  void _changeLocale() {
+    _locale = _locale == Locale('ru') ? const Locale('en') : const Locale('ru');
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello, world!')));
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: _locale,
+      home: HelloWorld(changeLocale: _changeLocale),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  final VoidCallback changeLocale;
+
+  const HelloWorld({super.key, required this.changeLocale});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Text(AppLocalizations.of(context).greetings),
+            IconButton(
+              onPressed: changeLocale,
+              icon: const Icon(Icons.language),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
